f8f04913a821171b3f82ac8a2b5bd20ae471252a
Parin <parinprojects@gmail.com>
2025-03-11 23:16:27 +0700

    feat: implement hero section with responsive design

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import Navbar from './components/Navbar.tsx'
+import Hero from './components/Hero.tsx'
 function App() {
   return (
-    <div className='w-full bg-white h-1000'>
+    <div className='w-full  h-1000'>
       <Navbar />
+      <Hero />
     </div>
   )
 }
